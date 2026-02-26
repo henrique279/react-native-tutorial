ded00435582b6e4e8856c6acce5b465d25c4a388
henrique <henriquejoshua63@gmail.com>
2026-02-26 14:29:11 -0300

    Primeiro commit

--- src/routes/main.ts
+++ src/routes/main.ts
@@ -1,7 +1,23 @@
 import { Router } from 'express';
+import { prisma } from '../libs/prisma';
 
 export const mainRouter = Router();
 
 mainRouter.get('/ping', (req, res) => {
     res.json({ pong: true });
+});
+
+mainRouter.get('/test', (req, res) => {
+    res.json({ testando: true });
+});
+
+mainRouter.post('/user', async (req, res) => {
+    const user = await prisma.user.create({
+        data: {
+            name: 'Billy Bob',
+            email: 'billy.bob@example.com'
+        }
+    });
+
+    res.json(user);
 });
